5602372fc89c8fed62f08bd68e97266e9e57ab41
Arthur Correnson <arthur.correnson@gmail.com>
2018-09-08 19:04:06 +0200

    improve verification script with colors

--- verify.py
+++ verify.py
@@ -12,12 +12,14 @@ print("--- Starting verification ---")
 error = False
 
 for i in range(len(needed)):
-  print("| verifying file " + needed[i] + "...")
+  print("| verifying file " + "\033[35m" + needed[i] + "\033[0m" + "..." )
   if os.path.exists("./docs/" + needed[i]):
-    print("| done -> " + u'\u2713')
+    print("| done -> " + "\033[92m" + u'\u2713' + "\033[0m")
   else:
-    print("| missing -> " + u'\u2717')
+    print("| missing -> " + "\033[31m" + u'\u2717' + "\033[0m")
     error = True
 
 if error:
   print("You need to add 'miniDna' module to your PYTHONPATH before generating the doc")
+else:
+  print("--- Verification ends successfully ---")
